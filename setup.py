3e8fce67e3dcca8f865691998ddbf4a26ad1631c
Stan Bergevoet <stanbergevoet@gmail.com>
2022-03-10 15:02:48 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='splicecopy',
     version='0.1',
@@ -10,4 +13,13 @@ setup(
     package_dir={'': 'src'},
     url='https://github.com/subzjee/splicecopy',
     keywords='copy fastcopy linux',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="Copy files using `splice` syscall",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Linux",
+    ]
+    python_requires=">=3.10"
 )
