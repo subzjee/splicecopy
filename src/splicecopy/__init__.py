6892c155941ea0df05352befdbc69978f87cc46e
Stan Bergevoet <stanbergevoet@gmail.com>
2022-05-15 21:39:40 +0200

    Added docstring

--- src/splicecopy/__init__.py
+++ src/splicecopy/__init__.py
@@ -8,12 +8,17 @@ class IsDirectoryError(Exception):
 
 class InvalidPlatformError(Exception):
     def __init__(self):
-        super().__init__(f"Invalid Platform. Requires Linux with Python >=3.10.")
+        super().__init__(f"Invalid Platform. Requires Linux with Python >= 3.10.")
 
 # `os.splice()` is only available on Linux and Python >=3.10
 _HAS_OS_SPLICE = hasattr(os, "splice") and sys.platform.startswith("linux")
 
-def copy(src: str, dst: str) -> str: 
+def copy(src: str, dst: str) -> str:
+    """Copy data from one fd to another using the splice(2)
+    syscall.
+    This requires the Linux kernel to be of version >= 2.6.17 and
+    glibc to be of version >= 2.5.
+    """
     if not _HAS_OS_SPLICE:
         raise InvalidPlatformError()
 
@@ -29,9 +34,11 @@ def copy(src: str, dst: str) -> str:
 
     with (open(src, 'rb') as fsrc,
           open(dst, 'wb') as fdst):
-        read_fd, write_fd = os.pipe()
-        for _ in range(int(os.path.getsize(src) / 65536) + 1):
-            os.splice(fsrc.fileno(), write_fd, os.path.getsize(src))
-            fdst.write(os.read(read_fd, os.path.getsize(src)))
+        infd, outfd = os.pipe()
+        fsrc_size = os.fstat(fsrc.fileno()).st_size
+
+        for _ in range(int(fsrc_size / 65536) + 1):
+            os.splice(fsrc.fileno(), outfd, fsrc_size)
+            fdst.write(os.read(infd, fsrc_size))
 
     return dst
